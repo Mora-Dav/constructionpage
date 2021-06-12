e924578e52ae003addadc8e7ef9ca511b55a680e
Mora-Dav <moradavi@gmail.com>
2021-06-11 21:38:38 -0300

    agrego imagen al inicio y cambio color del fondo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import wave from './waveGeneration1.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={wave} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
